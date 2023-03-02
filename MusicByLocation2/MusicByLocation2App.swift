98becc1162acaada0e4f8bfb17c2532797debf51
MikhailLukyanov1 <lukyanov.m@etoncollege.org.uk>
2023-03-02 17:30:38 +0000

    Initial Commit

---
+++ MusicByLocation2/MusicByLocation2App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocation2App.swift
+//  MusicByLocation2
+//
+//  Created by MIKHAEL LUKYANOV on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocation2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
